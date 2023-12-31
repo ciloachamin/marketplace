c0845b7ffada193954fba991c8178d466262528d
Christopher <christho987@hotmail.com>
2023-12-31 17:59:06 -0500

    Update for url

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
 			},
 			{
 				protocol: "https",
-				hostname: "digitalhippo-production.up.railway.app",
+				hostname: "marketplace-production-8aec.up.railway.app",
 			},
 		],
 	},
