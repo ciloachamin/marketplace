de65be1286ce06820491cf390edd2df44fe4f8ff
Christopher <christho987@hotmail.com>
2024-01-03 00:08:34 -0500

    update card for carucel price

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -58,17 +58,20 @@ const Card = ({
                             loading='eager'
                             src={validUrls[0]}
                             alt='Product image'
+                            className="h-full w-full"
                         />
+                        <div className="px-2  pb-1 absolute bottom-0 left-0  " >
+                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Precio: {formatPrice(product.price)}</span>
+                        </div>
                     </div>
 
-                    <div className="px-6 py-4  max-sm:py-0 ">
+                    <div className="px-6 pt-4  max-sm:py-0 ">
                         <div className="font-bold text-xl mb-2">{product.name}</div>
-                        <p className="text-secondary-foreground text-base">
+                        <p className="text-secondary-foreground text-base overflow-y-auto h-16" >
                             {product.description}
                         </p>
                     </div>
-                    <div className="px-6 pt-4 pb-2">
-                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Precio: {formatPrice(product.price)}</span>
+                    <div className="px-6 pt-2 pb-2" >
                         {typeof product.user === 'object' && product.user !== null ? (
                             <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
 
@@ -78,7 +81,7 @@ const Card = ({
                             <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">No hay información del vendedor disponible.</span>
                         )}
 
-                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Categoría: {label}</span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{label}</span>
                     </div>
                 </div>
             </Link>
@@ -89,7 +92,7 @@ const Card = ({
 const ProductPlaceholder = () => {
     return (
         <div className='max-w-sm  h-full bg-secondary overflow-hidden shadow-lg'>
-                <Skeleton className='h-full w-full' />
+            <Skeleton className='h-full w-full' />
 
         </div>
     )
