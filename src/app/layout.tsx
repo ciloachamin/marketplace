14b52df22bcd2f4fd2f3fb31813bf3d5a3f8b552
Christopher <christho987@hotmail.com>
2024-02-28 23:48:58 -0500

    remove google analitycs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,10 +20,10 @@ export default function RootLayout({
   return (
     <html lang='es' suppressHydrationWarning className='h-full'>
       <head>
-        {process.env.NODE_ENV === 'production' && (
+        {/* {process.env.NODE_ENV === 'production' && (
           <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2148072584206249"
             crossOrigin="anonymous"></script>
-        )}
+        )} */}
 
       </head>
       <body
