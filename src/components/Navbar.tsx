248bdc7544a73c6d0350c33c2ab302adcd0dd518
Christopher <christho987@hotmail.com>
2024-03-01 21:47:20 -0500

    carrucel

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -26,7 +26,7 @@ const Navbar = async () => {
 
               <div className='ml-4  max-sm:mx-2 flex lg:ml-0 h-15 w-20  max-sm:w-15'>
                 <Link href='/'>
-                  <Icons.logo className='w-full '/>
+                  <Icons.logo className='w-full ' />
                 </Link>
               </div>
 
@@ -35,6 +35,7 @@ const Navbar = async () => {
               </div>
               <div className='ml-auto flex items-center'>
                 <Search />
+                {/* <Search userId={"65945511053c95a8d160025d"} /> */}
               </div>
 
               <div className='ml-auto flex items-center'>
