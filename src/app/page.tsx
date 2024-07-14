78ec1872581918d9c4afca5347e8805e5e680f08
Christopher <christho987@hotmail.com>
2024-07-13 22:00:07 -0500

    Mejorar  rendimiento 5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -137,7 +137,7 @@ export default function Home() {
             className={buttonVariants()}>
             Explorar mas productos
           </Link> */}
-          <a aria-label="Explorar mas productos" class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2" href="/products">Explorar mas productos</a>
+          <a aria-label="Explorar mas productos" className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2" href="/products">Explorar mas productos</a>
         </div>
 
       </MaxWidthWrapper>
